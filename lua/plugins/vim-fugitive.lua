04e680a1572e63d976c066c4ca783e08f113ac35
Noah <noahskim7@gmail.com>
2025-05-31 18:17:55 -0700

    changed :Git binding to :0Git for fullscreen

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"tpope/vim-fugitive",
 		config = function()
-			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+			vim.keymap.set("n", "<leader>gs", ":0Git<CR>")
 			vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit!<CR>", { desc = "Git 3-way diff" })
 
 			local Boat_Fugitive = vim.api.nvim_create_augroup("Boat_Fugitive", {})
